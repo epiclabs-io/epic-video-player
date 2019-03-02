0e16e0d10368fbee15405b2f8d313adae210071a
jeoliva <jesus@epiclabs.io>
2019-03-02 20:18:57 +0100

    Add player native support (MP4)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { IPlayerConfig } from './models';
 import { PlayerDash } from './PlayerDash';
 import { PlayerHls } from './PlayerHls';
+import { PlayerNative } from './PlayerNative';
 
 export * from './models';
 export * from './Player';
@@ -8,12 +9,24 @@ export * from './PlayerHls';
 export * from './PlayerDash';
 
 export function newPlayer(url: string, htmlVideo: HTMLVideoElement, config?: IPlayerConfig) {
-  const filename = url.substr(url.lastIndexOf('/') + 1);
-  const extension = filename.split('.').pop();
-
-  if (extension === 'm3u8') {
-    return new PlayerHls(url, htmlVideo, config);
+  if (config && config.type) {
+    if (config.type === 'application/dash+xml') {
+      return new PlayerDash(url, htmlVideo, config);
+    } else if (config.type === 'application/x-mpegURL') {
+      return new PlayerHls(url, htmlVideo, config);
+    } else {
+      return new PlayerNative(url, htmlVideo, config);
+    }
   } else {
-    return new PlayerDash(url, htmlVideo, config);
+    const filename = url.substr(url.lastIndexOf('/') + 1);
+    const extension = filename.split('.').pop();
+
+    if (extension === 'm3u8') {
+      return new PlayerHls(url, htmlVideo, config);
+    } else if (extension === 'mpd') {
+      return new PlayerDash(url, htmlVideo, config);
+    } else {
+      return new PlayerNative(url, htmlVideo, config);
+    }
   }
 }
