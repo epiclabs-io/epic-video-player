0cb432965b9f32ce3f401a31276a0a38abf14cd9
Adrián Caballero <adrian@epiclabs.io>
2019-11-03 19:18:22 +0100

    Adding extra checks to player native methods

--- src/player-native.ts
+++ src/player-native.ts
@@ -17,27 +17,23 @@ export class PlayerNative extends Player<HTMLVideoElement> {
     }
 
     public destroy(): void {
-        try {
-            this.htmlPlayer.src = '';
-        } catch (e) {
-            console.warn(e);
-        } finally {
-            this.playerType = undefined;
-        }
+        this.htmlPlayer.src = '';
+        this.playerType = undefined;
     }
 
     public getRenditions(): IRendition[] {
         let renditions: IRendition[];
 
-        if (this.htmlPlayer.videoTracks && this.htmlPlayer.videoTracks.length) {
+        if (this.htmlPlayer.videoTracks && this.htmlPlayer.videoTracks.length > 0) {
             renditions = [];
+            // tslint:disable-next-line:prefer-for-of
             for (let i = 0; i < this.htmlPlayer.videoTracks.length; i++) {
-                const vt = this.htmlPlayer.videoTracks.item(i);
+                const vt = this.htmlPlayer.videoTracks[i];
                 if (vt) {
                     renditions.push({
                         bitrate: 0,
                         height: this.htmlPlayer.videoHeight,
-                        name: vt.label,
+                        name: vt.label || this.htmlPlayer.videoHeight.toString(),
                         width: this.htmlPlayer.videoWidth,
                     });
                 }
@@ -47,15 +43,16 @@ export class PlayerNative extends Player<HTMLVideoElement> {
         return renditions;
     }
 
-    public setRendition(rendition: number | IRendition, immediately: boolean): void {
-        if (this.htmlPlayer.videoTracks) {
+    public setRendition(rendition: number | IRendition, immediately?: boolean): void {
+        if (this.htmlPlayer.videoTracks && this.htmlPlayer.videoTracks.length > 0) {
             if (typeof rendition === 'number') {
                 if (rendition < this.htmlPlayer.videoTracks.length) {
-                    this.htmlPlayer.videoTracks.item(rendition).selected = true;
+                    this.htmlPlayer.videoTracks[rendition].selected = true;
                 }
             } else {
+                // tslint:disable-next-line:prefer-for-of
                 for (let i = 0; i < this.htmlPlayer.videoTracks.length; i++) {
-                    const vt = this.htmlPlayer.videoTracks.item(i);
+                    const vt = this.htmlPlayer.videoTracks[i];
                     if (rendition.name && vt.label === rendition.name) {
                         vt.selected = true;
                     }
@@ -65,15 +62,15 @@ export class PlayerNative extends Player<HTMLVideoElement> {
     }
 
     public getCurrentRendition(): IRendition {
-        if (this.htmlPlayer.videoTracks) {
-            if (this.htmlPlayer.videoTracks.selectedIndex < this.htmlPlayer.videoTracks.length) {
-                return {
-                    bitrate: 0,
-                    height: this.htmlPlayer.videoHeight,
-                    name: this.htmlPlayer.videoTracks.item(this.htmlPlayer.videoTracks.selectedIndex).label,
-                    width: this.htmlPlayer.videoWidth,
-                };
-            }
+        if (this.htmlPlayer.videoTracks && this.htmlPlayer.videoTracks.length > 0 &&
+          this.htmlPlayer.videoTracks.selectedIndex < this.htmlPlayer.videoTracks.length) {
+            const track = this.htmlPlayer.videoTracks[this.htmlPlayer.videoTracks.selectedIndex];
+            return {
+                bitrate: 0,
+                height: this.htmlPlayer.videoHeight,
+                name: track.label || '',
+                width: this.htmlPlayer.videoWidth,
+            };
         }
 
         return undefined;
