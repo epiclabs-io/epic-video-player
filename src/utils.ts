5950b705a8a8ae29146577f9553fb555a57864e6
Adrian Caballero <adripanicodev@gmail.com>
2023-07-25 18:51:38 +0200

    refactor: more linting

--- src/utils.ts
+++ src/utils.ts
@@ -14,22 +14,30 @@ export function getDroppedFrames(htmlVideo: HTMLVideoElement): number {
 }
 
 export function getDuration(htmlVideo: HTMLVideoElement): number {
-  if (!htmlVideo) { return; }
+  if (!htmlVideo) {
+    return;
+  }
   return htmlVideo.duration;
 }
 
 export function getBuffered(htmlVideo: HTMLVideoElement): TimeRanges {
-  if (!htmlVideo) { return; }
+  if (!htmlVideo) {
+    return;
+  }
   return htmlVideo.buffered;
 }
 
 export function getSeekable(htmlVideo: HTMLVideoElement): TimeRanges {
-  if (!htmlVideo) { return; }
+  if (!htmlVideo) {
+    return;
+  }
   return htmlVideo.seekable;
 }
 
 export function getPlayed(htmlVideo: HTMLVideoElement): TimeRanges {
-  if (!htmlVideo) { return; }
+  if (!htmlVideo) {
+    return;
+  }
   return htmlVideo.played;
 }
 
@@ -37,7 +45,7 @@ export function timeRangesToITimeRanges(tr: TimeRanges): ITimeRanges[] {
   const res: ITimeRanges[] = [];
   if (tr && tr.length > 0) {
     for (let i = 0; i < tr.length; i++) {
-      res.push({start: tr.start(i), end: tr.end(i)});
+      res.push({ start: tr.start(i), end: tr.end(i) });
     }
   }
   return res;
